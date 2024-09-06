810be8108ca709b465b070559d878aafa9016923
Steven <stevenaknobel@gmail.com>
2024-09-06 09:45:55 +0200

    completed the 5 challenges using Ternary operators

--- index.js
+++ index.js
@@ -5,14 +5,24 @@
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
+//Solution to Challenge 1
+//Ternary operator structure -- (condition ? value if true : value if false)
+//!== strict inequality operator (checks both value and type)
+//cannot use just = as it is like let a = 5, rather than doing a comparison
+//userPreference !== undefined <checks if user preference is not equal to undefined
+//userPreference : "Regular" <if it is not undefined then takes the userPreference, otherwise "Regular" is used
+let preference = userPreference !== undefined ? userPreference : "Regular";
+console.log(preference);
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
 let cansLeft = 3; // Any number of cans
-console.log(``);
+//Solution to Challenge 2
+//Ternary operator structure -- (condition ? value if true : value if false)
+let stockCheck = cansLeft < 5 ? "Time to restock!" : "We're stocked!";
+console.log(stockCheck);
 
 
 // #### Challenge 3: Workout Intensity
@@ -20,7 +30,10 @@ console.log(``);
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
 
 let heartRate = 95; // Current heart rate in bpm
-console.log(`'}`);
+//Solution to Challenge 3
+//Ternary operator structure -- (condition ? value if true : value if false)
+let workoutIntensity = heartRate < 100 ? "Boost needed!" : "Energy levels are high!";
+console.log(workoutIntensity);
 
 
 // #### Challenge 4: Temperature Suitability
@@ -28,7 +41,11 @@ console.log(`'}`);
 // if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 
 let currentTemp = 4; // Current temperature in °C
-console.log(``);
+//Solution to Challenge 4
+//Ternary operator structure -- (condition ? value if true : value if false)
+//can also be written as let temperatureSuitability = currentTemp <= 5 ? "Chilled to perfection!" : "Needs a cool!";
+let temperatureSuitability = currentTemp > 5 ? "Needs a cool!" : "Chilled to perfection!";
+console.log(temperatureSuitability);
 
 
 // #### Challenge 5: Late Night Coding Session
@@ -43,8 +60,11 @@ let currentHour = 22;
 // If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
 // Otherwise, print "Better stick to water."
 
-
-
+//Solution to Challenge 5
+//Ternary operator structure -- (condition ? value if true : value if false)
+//&& logical operator used to check BOTH conditions are true, otherwise false
+let lateNightCoding = (currentHour >= 7 && currentHour <24) ? "Unleash the beast!" : "Better stick to water.";
+console.log(lateNightCoding);
 
 
 
